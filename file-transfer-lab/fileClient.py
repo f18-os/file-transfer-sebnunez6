fab94e649fc857398345b9e41adb68a7345a9b2c
sebnunez6 <snunez6@miners.utep.edu>
2018-09-30 10:02:51 +0000

    Files created for Server and Client

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3
+
+# Echo client program
+import socket, sys, re, os
+
+sys.path.append("../lib")       # for params
+import params
+
+from framedSock import framedSend, framedReceive
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "fileClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+files = os.listdir(os.curdir)  #grab current files
+print(files)
+inputFile = input("Please select a file to send \n")
+with open(inputFile.strip(),"rb") as binary_file:
+    # Read the whole file at once
+    data = binary_file.read()
+    if len(data) < 1:
+        print("File contents empty")
+        sys.exit(0)
+    for line in data.split(b'\n'):
+        line = b':' + line
+        print(line)
+        framedSend(s,line, debug)
+        print("received" ,framedReceive(s,debug))
+
+

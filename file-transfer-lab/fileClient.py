519cdf3b0760b599ffdec33ef9b93c926583b049
sebnunez6 <snunez6@miners.utep.edu>
2018-10-01 18:03:52 +0000

    Sending and receiving files implemented

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -58,20 +58,32 @@ if s is None:
 files = os.listdir(os.curdir)  #grab current files
 print(files)
 inputFile = input("Please select a file to send \n")
-with open(inputFile.strip(),"rb") as binary_file:
-    # Read the whole file at once
-    data = binary_file.read()
+#Attemps to read input as file
+try:
+    with open(inputFile.strip(),"rb") as binary_file:
+        # Read the whole file at once
+        data = binary_file.read()
+except FileNotFoundError:
+    print("file not found exiting")
+    sys.exit(0)
 
-    #check if file exists
-    if len(data) < 1:
-        print("File contents empty")
-        sys.exit(0)
+#sends file information to server
+framedSend(s,b':' +inputFile.strip().encode('utf-8') + b"\'start\'")
 
-    #sends info to server
-    for line in data.split(b'\n'):
-        line = b':' +inputFile.strip().encode('utf-8') +"\'start \'".encode('utf-8')+ line
-        print(line)
-        framedSend(s,line, debug)
-        print("received" ,framedReceive(s,debug))
+#Gets rid of new line character due to sending error by replacing it with 'e'
+data = data.replace(b"\n", b"\'e\'")
+
+#Sends 100 bits at a time to server
+while len(data) >= 100:
+    line = data[:100]
+    data = data[100:]
+    framedSend(s,b":"+line,debug)
+
+#sends left over bits
+if len(data) > 0:
+    framedSend(s,b":"+data,debug)
+
+#tells server file has ended
+framedSend(s,b":\'end\'")
 
 
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -2,7 +2,7 @@
 import sys, re, socket, os 
 sys.path.append("../lib")       # for params
 import params
-
+os.chdir("Server")
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
     (('-d', '--debug'), "debug", False), # boolean (set if present)
@@ -25,17 +25,56 @@ while True:
     print("listening on:", bindAddr)
 
     sock, addr = lsock.accept()
+    
+    #creates child to handle accepted connection
     rc = os.fork()
     if rc == 0:
         print("connection rec'd from", addr)
 
-
         from framedSock import framedSend, framedReceive
 
+        start = framedReceive(sock, debug)
+        try:
+            start = start.decode()
+        except AttributeError:
+            print("error exiting: ", start)
+            sys.exit(0)
+        
+        #gets rid of any characters not involved with the filename
+        count = 0
+        for char in start:
+                    if char.isalpha():
+                        break
+                    else:
+                        count = count + 1
+        start = start[count:]
+
+        #tells server where file name ends
+        start = start.split("\'start\'")
+
+        #opening file
+        file = open(start[0].strip(),"wb+")
+
+        #Receives input while file has not ended
         while True:
-            payload = framedReceive(sock, debug)
+            #error handling
+            try:
+                payload = framedReceive(sock, debug)
+                #special characters could not be passed new lines where replaced with 'e'. Converting them back
+                payload = payload.replace(b"\'e\'",b"\n")
+            except:
+                pass
+
+            #checking for debugging
             if debug: print("rec'd: ", payload)
             if not payload:
                 break
-            framedSend(sock, payload, debug)
+            #checking if end of file else writes to file
+            if b"\'end\'" in payload:
+                file.close()
+                sys.exit(0)
+            else:
+                file.write(payload[1:])
+
+        #ensures child exits while loop
         break
